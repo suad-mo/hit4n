d76c96f9af22105fb166da67d931b1762dace162
Suad Krvavac <suad.krvavac@gmail.com>
2021-10-18 16:21:20 +0200

    Change Option Page

--- src/app/hits/options/options.page.ts
+++ src/app/hits/options/options.page.ts
@@ -1,9 +1,10 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
-import { Storage } from '@capacitor/storage';
 import { Dialog } from '@capacitor/dialog';
-import { from, Observable, of, Subscription } from 'rxjs';
+import { AlertController } from '@ionic/angular';
+import { Observable, Subscription } from 'rxjs';
+import { map, tap } from 'rxjs/operators';
+import { Hit, HitGame } from '../hits.model';
 import { HitsService } from '../hits.service';
-import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-options',
@@ -12,16 +13,30 @@ import { map } from 'rxjs/operators';
 })
 export class OptionsPage implements OnInit, OnDestroy {
   currentGamer$ = new Observable<string>();
+  topTenGames$ = new Observable<HitGame[]>();
+
   toogleCheck = false;
+  isDelete = false;
+  isLoad = true;
 
   private sub: Subscription;
+  private sub1: Subscription;
 
   constructor(
-    private hitService: HitsService
+    private hitService: HitsService,
+    private alertCtrl: AlertController,
   ) { }
 
   ngOnInit() {
     this.currentGamer$ = this.hitService.currentGamer;
+    this.sub1 = this.hitService.topTenGames
+    .pipe(
+      map(games => games.length > 0)
+    )
+    .subscribe(bool => {
+      this.isDelete = bool;
+      this.isLoad = !bool;
+    });
   }
 
   onChangeGamer() {
@@ -42,9 +57,44 @@ export class OptionsPage implements OnInit, OnDestroy {
       });
   };
 
+  onClearTopTenGames() {
+    this.presentAlertConfirm()
+      .then(() => this.isDelete = false);
+  }
+
+  async presentAlertConfirm() {
+    const alert = await this.alertCtrl.create({
+      cssClass: 'my-custom-class',
+      header: 'Confirm!',
+      message: 'Are you sure you want to delete <strong>Top Ten Games </strong>!!!',
+      buttons: [
+        {
+          text: 'Cancel',
+          role: 'cancel',
+          cssClass: 'secondary',
+        }, {
+          text: 'Ok',
+          handler: () => {
+            this.hitService.clearTopTenGames();
+          }
+        }
+      ]
+    });
+
+    await alert.present();
+  }
+
+  onLoadTopTenGames() {
+    this.hitService.onLoadTopTenGames();
+    this.isLoad = false;
+  }
+
   ngOnDestroy() {
     if (this.sub) {
       this.sub.unsubscribe();
+    }
+    if (this.sub1) {
+      this.sub1.unsubscribe();
     }
   }
 
